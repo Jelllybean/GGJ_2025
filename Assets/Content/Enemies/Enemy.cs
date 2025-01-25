0fc7203cd0291c2bb15cd2e0ff83e32af67f23c0
Lea <39883637+Roboolet@users.noreply.github.com>
2025-01-25 08:35:27 +0100

    Re-added BigBubble and fixed layer of enemy

--- Assets/Content/Enemies/Enemy.cs
+++ Assets/Content/Enemies/Enemy.cs
@@ -5,6 +5,8 @@ public class Enemy : Agent
 {
     [HideInInspector]
     public List<GameObject> smallBubbles = new List<GameObject>();
+
+    [SerializeField] private GameObject bigBubble;
     private int bubbleCount = 0;
     void Start()
     {
@@ -22,7 +24,7 @@ public class Enemy : Agent
         smallBubbles.Add(_smallBubble);
         if(bubbleCount > 3)
         {
-            transform.GetChild(0).gameObject.SetActive(true);
+            bigBubble.SetActive(true);
             for (int i = 0; i < smallBubbles.Count; i++)
             {
                 smallBubbles[i].SetActive(false);
