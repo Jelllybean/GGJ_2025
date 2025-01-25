db8ebc233a5f203f4a5f5d8f354b1771d00dd673
Lea <39883637+Roboolet@users.noreply.github.com>
2025-01-25 11:04:36 +0100

    Added speed decrease mechanic

--- Assets/Content/Enemies/Enemy.cs
+++ Assets/Content/Enemies/Enemy.cs
@@ -1,28 +1,31 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : Agent
 {
+    [SerializeField] private NavMeshAgent navAgent;
+    
     [Header("Bubble mechanics")]
     [SerializeField] private GameObject bigBubble;
+    [SerializeField] private int bubblesUntilBigBubble;
+    [SerializeField] private float minimumSpeed, maximumSpeed;
+    
     [HideInInspector] public List<GameObject> smallBubbles = new List<GameObject>();
 
     private int bubbleCount = 0;
-    void Start()
+
+    private void Start()
     {
-
-    }
-
-    void Update()
-    {
-
+        navAgent.speed = maximumSpeed;
     }
 
     public void AttachBubble(GameObject _smallBubble)
     {
         bubbleCount++;
         smallBubbles.Add(_smallBubble);
-        if(bubbleCount > 3)
+        if(bubbleCount >= bubblesUntilBigBubble)
         {
             bigBubble.SetActive(true);
             for (int i = 0; i < smallBubbles.Count; i++)
@@ -32,6 +35,10 @@ public class Enemy : Agent
             smallBubbles.Clear();
             _smallBubble.SetActive(false);
         }
+        
+        // reduce speed with each bubble
+        navAgent.speed = Mathf.Lerp(maximumSpeed, minimumSpeed,
+            ((float)bubbleCount / (float)bubblesUntilBigBubble));
     }
 
     //public IEnumerator BigBubbleCountDown()
