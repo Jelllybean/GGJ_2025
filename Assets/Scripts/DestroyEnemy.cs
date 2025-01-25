24bc589af3714c96d75ef6886f2f412c19487989
Emily <emilyvdschaaf@gmail.com>
2025-01-25 15:06:05 +0100

    oops i forgor

--- Assets/Scripts/DestroyEnemy.cs
+++ Assets/Scripts/DestroyEnemy.cs
@@ -9,15 +9,16 @@ public class DestroyEnemy : MonoBehaviour
 	void Start()
 	{
 		rayfireRigid.DemolishForced();
-		bomb.transform.SetParent(null);
-		bomb.transform.position = rayfireRigid.fragments[0].transform.parent.position;
+		transform.SetParent(null);
+		transform.position = rayfireRigid.fragments[0].transform.parent.position;
 		rayfireRigid.fragments[0].transform.parent.SetParent(transform);
 		this.enabled = true;
 		bomb.enabled = true;
 	}
 
-	public void ExplodeEnemy()
+	public void ExplodeEnemy(Transform _enemyPosition)
 	{
+		transform.position = _enemyPosition.position;
 		transform.GetChild(0).gameObject.SetActive(true);
 		bomb.Explode(0);
 		Invoke("Reset", 1.5f);
