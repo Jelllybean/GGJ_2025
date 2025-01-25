d45716c6d06d545999773e43c68c42ca5331b8c8
Emily <emilyvdschaaf@gmail.com>
2025-01-25 15:04:27 +0100

    enemy destruction ready

--- Assets/Scripts/DestroyEnemy.cs
+++ Assets/Scripts/DestroyEnemy.cs
@@ -6,26 +6,26 @@ public class DestroyEnemy : MonoBehaviour
 	public RayfireRigid rayfireRigid;
 	public RayfireBomb bomb;
 
-	// Start is called once before the first execution of Update after the MonoBehaviour is created
 	void Start()
 	{
 		rayfireRigid.DemolishForced();
 		bomb.transform.SetParent(null);
 		bomb.transform.position = rayfireRigid.fragments[0].transform.parent.position;
-	}
-
-	// Update is called once per frame
-	void Update()
-	{
-		if (Input.GetKeyDown(KeyCode.J))
-		{
-			ExplodeEnemy();
-		}
+		rayfireRigid.fragments[0].transform.parent.SetParent(transform);
+		this.enabled = true;
+		bomb.enabled = true;
 	}
 
 	public void ExplodeEnemy()
 	{
-		rayfireRigid.Activate();
+		transform.GetChild(0).gameObject.SetActive(true);
 		bomb.Explode(0);
+		Invoke("Reset", 1.5f);
+	}
+
+	public void Reset()
+	{
+		bomb.Restore();
+		transform.GetChild(0).gameObject.SetActive(false);
 	}
 }
