14fe515d2dce2a195bbfbea879fc3890dc6f93fe
Lea <39883637+Roboolet@users.noreply.github.com>
2025-01-26 00:32:28 +0100

    Set up new Player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,38 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Aiming")]
+    [SerializeField] private Camera camera;
+    [SerializeField] private float sensitivity;
+    
+    [Header("Movement")]
+    [SerializeField] private CharacterController controller;
+    [SerializeField] private float moveSpeed;
+
+    [Header("Visuals")] 
+    [SerializeField] private Animator animator;
+    
+    
+    public void Input_Move(CallbackContext input)
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    public void Input_Aim(CallbackContext input)
+    {
+        
+    }
+    public void Input_Bubble(CallbackContext input)
+    {
+        
+    }
+    public void Input_Dart(CallbackContext input)
+    {
+        
+    }
+    public void Input_Pause(CallbackContext input)
     {
         
     }
