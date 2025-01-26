f47bf755f19c92adbfdebbf3372c0fa03bc7c9d4
Lea <39883637+Roboolet@users.noreply.github.com>
2025-01-26 01:11:14 +0100

    Simple movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,24 +1,44 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 using static UnityEngine.InputSystem.InputAction;
 
 public class Player : MonoBehaviour
 {
     [Header("Aiming")]
-    [SerializeField] private Camera camera;
     [SerializeField] private float sensitivity;
+    private Camera camera;
     
     [Header("Movement")]
     [SerializeField] private CharacterController controller;
-    [SerializeField] private float moveSpeed;
+    [SerializeField] private float acceleration, friction, gravityAccel;
+    private Vector2 inputDir;
+    private Vector3 velocity;
 
     [Header("Visuals")] 
     [SerializeField] private Animator animator;
-    
-    
+
+    private void Awake()
+    {
+        camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        velocity += camera.transform.forward * (inputDir.y * acceleration * Time.deltaTime);
+        velocity += camera.transform.right * (inputDir.x * acceleration * Time.deltaTime);
+        if(!controller.isGrounded) velocity += Vector3.down * (gravityAccel * Time.deltaTime);
+
+        controller.Move(velocity);
+
+        velocity -= new Vector3(velocity.x * friction * Time.deltaTime, velocity.y,
+            velocity.z * friction * Time.deltaTime);
+    }
+
     public void Input_Move(CallbackContext input)
     {
-        
+        inputDir = input.ReadValue<Vector2>();
     }
     public void Input_Aim(CallbackContext input)
     {
