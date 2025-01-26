74d55948ab2e6c0f16e89647eebb814626dd8e1e
Lea <39883637+Roboolet@users.noreply.github.com>
2025-01-26 09:15:01 +0100

    OopyGoopy Behaviour

---
+++ Assets/Scripts/Behaviour/OopyGoopyBlueprint.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "BehaviourBlueprint/OopyGoopy", order = -10000)]
+public class OopyGoopyBlueprint: BehaviourBlueprint
+{
+    public float stepTime;
+    public float rangedDistance;
+    public LayerMask playerLayerMask;
+    private const string BB_TARGET = "enemy_target";
+    private const string BB_TARGET_DISTANCE = "enemy_target_distance";
+    private const string BB_TARGET_WILL_SHOOT = "enemy_target_shoot";
+    
+    public override INode BuildTree()
+    {
+        return new NCSequence(new INode[]
+        {
+            new NGetGameObjectWithTag("Player", BB_TARGET),
+            new NGetDistanceTo(BB_TARGET, BB_TARGET_DISTANCE, PositionReadMode.GAME_OBJECT),
+            
+            // if target is nearby, shoot
+            new NCSelector(new INode[]
+            {
+                new NDComparison<float>(new NDHasLineOfSight(new NCSequence(new INode[]
+                        {
+                            new NPrint(PrintMode.LOG, "Shoot")
+                        }), 
+                        BB_TARGET, playerLayerMask),
+                    BB_TARGET_DISTANCE, Comparator.GREATER, rangedDistance, true),
+                
+                new NCSequence(new INode[]
+                {
+                    new NMoveTowards(BB_TARGET, PositionReadMode.GAME_OBJECT),
+                    new NPrint(PrintMode.LOG, "Move"),
+                    new NEnemyStep(),
+                    new NWait(stepTime)
+                })
+            }),
+            
+            
+            
+            new NBlackboardSet(BB_TARGET_WILL_SHOOT, false)
+            
+        });
+    }
+}
